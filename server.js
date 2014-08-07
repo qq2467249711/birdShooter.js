9d14043100b433dfcb0a500d26b5ec158ff77ffb
unknown <qq2467249711@gmail.com>
2014-08-07 15:50:24 -0400

    Hopefully  fixed :D

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection(process.env.OPENSHIFT_MYSQL_DB_URL);
 connection.connect(function(){
 app.use(bodyparser.json());
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP,function(){
-app.use("/",express.static("BS"));
+app.use("/",express.static("/"));
 app.post("/save", function(req,res){
 	console.log("INFO: Received "+JSON.stringify(req.body));
 	connection.query("select * from Users where Username =\""+req.body.username+"\" and Password=\""+req.body.password+"\"",function(err,rows){
