29e6ac22b4991556d051474c655fcd3e8e2fbd5b
unknown <qq2467249711@gmail.com>
2014-08-07 12:41:33 -0400

    fix process.env

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var mysql = require("mysql");
-var connection = mysql.createConnection(global["process.env.OPENSHIFT_"+process.env.OPENSHIFT_APP_NAME.toUppercase()+"_DB_URL"]);
+function appname(){
+	var s = new String;
+	s = process.env.OPENSHIFT_APP_NAME;
+	return s.toUppercase();
+}
+var connection = mysql.createConnection(global["process.env.OPENSHIFT_"+appname()+"_DB_URL"]);
 connection.connect(function(){
 app.use(bodyparser.json());
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, function(){
