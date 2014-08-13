e2d7385f46d232d599c9b187bbf2b96285e960c5
unknown <qq2467249711@gmail.com>
2014-08-12 21:12:12 -0400

    cool password field v3

--- BSclient.js
+++ BSclient.js
@@ -608,7 +608,7 @@ function pp(){
 function dbp(i){
 	if(i>=players[0].password.length)
 		return Math.round((screen.width-30*8)/2+(i-1)*Math.round(birdWidth/10));
-    context.drawImage(birdI, 90, 219, Math.round(birdWidth/10), Math.round(birdHeight/10), Math.round((screen.width-30*8)/2+(i-1)*Math.round(birdWidth/10)), punby, birdWidth, birdHeight);
+    context.drawImage(birdI, 90, 219, birdWidth, birdHeight, Math.round((screen.width-30*8)/2+(i-1)*Math.round(birdWidth/10)), punby, Math.round(birdWidth/10), Math.round(birdHeight/10));
 	return dbp(i+1);
 }
 function pun(){
