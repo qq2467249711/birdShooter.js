a0332cbbbde3c20a330f267b2defe459363b8495
unknown <qq2467249711@gmail.com>
2014-08-12 21:41:48 -0400

    great/awesome/nice and good-looking password field and some bug fixes

--- BSclient.js
+++ BSclient.js
@@ -607,8 +607,8 @@ function pp(){
 }
 function dbp(i){
 	if(i>=players[0].password.length)
-		return Math.round((screen.width-30*8)/2+(i-1)*Math.round(birdWidth/5));
-    context.drawImage(birdI, 90, 219, birdWidth, birdHeight, Math.round((screen.width-30*8)/2+(i-1)*Math.round(birdWidth/10)), punby, Math.round(birdWidth/5), Math.round(birdHeight/5));
+		return Math.round((screen.width-30*8)/2+(i-1)*Math.round(birdWidth/2));
+    context.drawImage(birdI, 90, 219, birdWidth, birdHeight, Math.round((screen.width-30*8)/2+((i-1)*birdWidth/2)/players[0].password.length), punby-40, Math.round(birdWidth/2), Math.round(birdHeight/2));
 	return dbp(i+1);
 }
 function pun(){
